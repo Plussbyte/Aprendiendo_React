6b9c3e13800f94d308c7ce2510a3c87c55b0d507
Plussbyte <samuel.estrada139@pascualbravo.edu.co>
2024-10-18 07:50:56 -0500

    Actualizacion_2

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,35 @@
 
 const datos = {
-    nombre: 'Daniel',
-    apellidos: 'Correa',
-    edad: 19,
-    direccion:{
-        carrera: 80,
-        numero1: 116,
-        numero2: 46,
-        ciudad: 'medellin'
-    }
+    nombre:'daniel',
+    edad:23,
+    cedula:1000349323
 }
 
-// const datos2 = datos -->  nunca hacer esto.
+console.log(datos.nombre,datos.edad,datos.cedula) //--> no es la forma correcta de llamar las propiedades de un objeto (muy largo).
 
-console.log(datos)
-console.log(`La ciudad de ${datos.nombre} es ${datos.direccion.ciudad}`)
+const {nombre,edad,cedula} = datos //--> los {} indican que va a extraer de esta manera desestructura el objeto.
+
+console.log(nombre,edad,cedula)//--> de esta manera es mucho mas facil obtener datos.
+
+
+const datos2 = {...datos} //--> usamos un objeto literal
+
+const desestructuracion = ({nombre,edad,cedula,ID = '00001'})=>{ //--> de esta manera de desestructura en una funcion
+
+    console.log(nombre,edad,cedula,ID) 
+}//--> el ID es una variable las cuales podemos añadir junto con los otros parametros y asignarles un valor
+
+desestructuracion(datos2)
+
+
+const cambioNombres = ({nombre,edad,cedula})=>({
+    name: nombre,
+    age: edad,
+    ID: cedula 
+})
+
+const {name,age,ID} = cambioNombres(datos)
+
+
+
+console.log(name,age,ID)
