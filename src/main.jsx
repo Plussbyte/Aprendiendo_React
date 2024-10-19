6187f4a89de52bf9956b48b75f54f7f94fbc001d
Plussbyte <samuel.estrada139@pascualbravo.edu.co>
2024-10-19 06:56:09 -0500

    Actualizacion_3

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,14 @@
+const arreglo = ['Hola','como','estas']
 
-const datos = {
-    nombre:'daniel',
-    edad:23,
-    cedula:1000349323
+const [,,P3] = arreglo //--> de esta forma desestructuramos un arreglo 
+
+console.log(P3) //--> para llegar a una posicion del arreglo sin tener que asignar una variable se realisa por comas(,)
+
+
+const functionArreglo = ()=>{
+    return ['ABC',123]
 }
 
-console.log(datos.nombre,datos.edad,datos.cedula) //--> no es la forma correcta de llamar las propiedades de un objeto (muy largo).
+const [letras,numeros] = functionArreglo()
 
-const {nombre,edad,cedula} = datos //--> los {} indican que va a extraer de esta manera desestructura el objeto.
-
-console.log(nombre,edad,cedula)//--> de esta manera es mucho mas facil obtener datos.
-
-
-const datos2 = {...datos} //--> usamos un objeto literal
-
-const desestructuracion = ({nombre,edad,cedula,ID = '00001'})=>{ //--> de esta manera de desestructura en una funcion
-
-    console.log(nombre,edad,cedula,ID) 
-}//--> el ID es una variable las cuales podemos añadir junto con los otros parametros y asignarles un valor
-
-desestructuracion(datos2)
-
-
-const cambioNombres = ({nombre,edad,cedula})=>({
-    name: nombre,
-    age: edad,
-    ID: cedula 
-})
-
-const {name,age,ID} = cambioNombres(datos)
-
-
-
-console.log(name,age,ID)
+console.log(letras,numeros)
